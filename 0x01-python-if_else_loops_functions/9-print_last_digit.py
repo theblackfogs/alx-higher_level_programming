0739db7e1984d1b8251c40190e74edc399802f1d
theblackfogs <theblackfogs@gmail.com>
2024-01-03 17:56:10 -0800

    add last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+last = 0
+def print_last_digit(number):
+    if number < 0:
+        last = ((number * -1) % 10)
+    else:
+        last = number % 10
+    print(f"{last}", end="")
+    return last
+
